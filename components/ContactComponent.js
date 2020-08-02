bb350a0a87bbd6c82ad792ca1aa64f4986d91a4d
sumit <sharmasumit0308@gmail.com>
2020-08-02 21:44:40 +0530

    previous version

---
+++ components/ContactComponent.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import { Text, View,StyleSheet } from 'react-native';
+import { Card } from 'react-native-elements';
+
+class Contact extends Component{
+    constructor(props){
+        super(props);
+
+
+    }
+
+    render(){
+        const styles = StyleSheet.create({
+            container: {
+              marginTop: 50,
+            },
+            bigBlue: {
+              color: 'blue',
+              fontWeight: 'bold',
+              fontSize: 30,
+            },
+            red: {
+              color: 'red',
+            },
+          });
+        return(
+           <View style={styles.container}>
+               <Card>   
+                    <Text style={styles.red}>Contact Information</Text>
+                    <Text>Our Address</Text>
+                    <Text>121, Clear Water Bay Road
+                      Clear Water Bay, Kowloon
+                      HONG KONG
+                      Tel: +852 1234 5678
+                      Fax: +852 8765 4321
+                      Email:confusion@food.net
+                    </Text>
+                    
+                </Card>
+                
+           </View>
+           
+        );
+      
+      
+          
+    }
+    
+    
+}
+
+
+
+export default Contact;
--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,54 +1,131 @@
-import React, { Component } from 'react';
-import Menu from './MenuComponent';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { View, FlatList } from 'react-native';
-import { DISHES } from '../shared/dishes';
-import Home from './HomeComponent';
-import Dishdetail from './DishdetailComponent';
+import React, {Component} from 'react';
+import Menu from "./MenuComponent";
+import Dishdetail from "./DishdetailComponent";
+import {createAppContainer} from "react-navigation";
+import {createStackNavigator} from "react-navigation-stack";
+import Home from "./HomeComponent";
+import {createDrawerNavigator} from "react-navigation-drawer";
+import About from "./AboutComponent";
+import Contact from "./ContactComponent";
 
-
-function MenuRender() {
-  const Stack = createStackNavigator();
-  return (   
-   
-    <Stack.Navigator>
-          <Stack.Screen name="Menu" component={Menu } options={{
-            title: 'Menu',
+const MenuNavigator = createStackNavigator({
+        Menu: { screen: Menu },
+        Dishdetail: { screen: Dishdetail }
+    },
+    {
+        initialRouteName: 'Menu',
+        defaultNavigationOptions: {
             headerStyle: {
-              backgroundColor: '#f4511e',
+                backgroundColor: "#512DA8"
             },
             headerTintColor: '#fff',
             headerTitleStyle: {
-              fontWeight: 'bold',
-            },
-          }} />
-          <Stack.Screen name="Dishdetail" component={Dishdetail} options={{
-            title: 'Dish Details',
-            headerStyle: {
-              backgroundColor: '#f4511e',
-            },
-            headerTintColor: '#fff',
-            headerTitleStyle: {
-              fontWeight: 'bold',
-            },
-          }} />
-      </Stack.Navigator>
-
-  );
-}
-
-function Main(props){
-  const Drawer = createDrawerNavigator();
-  return(
-    <NavigationContainer>
-    <Drawer.Navigator initialRouteName="Home">
-      <Drawer.Screen name="Home" component={Home} />
-      <Drawer.Screen name="Menu" component={MenuRender} />
-      </Drawer.Navigator>
-  </NavigationContainer>           
+                color: "#fff"
+            }
+        }
+    }
 );
-}
-export default Main;
 
+const HomeNavigator = createStackNavigator({
+        Home: { screen: Home }
+    },
+    {
+        initialRouteName: 'Home',
+        defaultNavigationOptions: {
+            headerStyle: {
+                backgroundColor: "#512DA8"
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: "#fff"
+            }
+        }
+    }
+);
+
+
+const AboutNavigator = createStackNavigator({
+        About: { screen: About }
+    },
+    {
+        initialRouteName: 'About',
+        defaultNavigationOptions: {
+            headerStyle: {
+                backgroundColor: "#512DA8"
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: "#fff"
+            }
+        }
+    }
+);
+
+const ContactNavigator = createStackNavigator({
+        Contact: { screen: Contact }
+    },
+    {
+        initialRouteName: 'Contact',
+        defaultNavigationOptions: {
+            headerStyle: {
+                backgroundColor: "#512DA8"
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: "#fff"
+            }
+        }
+    }
+);
+
+
+const MainNavigator = createDrawerNavigator({
+    Home: {
+        screen: HomeNavigator,
+        navigationOptions: {
+            title: 'Home',
+            drawerLabel: 'Home'
+        }
+    },
+    About: {
+        screen: AboutNavigator,
+        navigationOptions: {
+            title: 'About Us',
+            drawerLabel: 'About Us'
+        }
+    },
+    Menu: {
+        screen: MenuNavigator,
+        navigationOptions: {
+            title: 'Menu',
+            drawerLabel: 'Menu'
+        }
+    },
+    Contact: {
+        screen: ContactNavigator,
+        navigationOptions: {
+            title: 'Contact Us',
+            drawerLabel: 'Contact Us'
+        }
+    }
+}, {
+    drawerBackgroundColor: '#D1C4E9'
+    }
+);
+
+
+// class Main extends Component{
+//
+//     render() {
+//         return (
+//             <View style={{flex:1, paddingTop: 0 }}>
+//                 <MenuNavigator/>
+//             </View>
+//         );
+//     }
+// }
+
+// export default Main;
+const Main = createAppContainer(MainNavigator);
+
+export default Main;
